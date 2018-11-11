b48ba1a84ddc9bd1379f26c9323b9ebdcd15950b
rlcapunpon <capunponrlc@gmail.com>
2018-11-11 17:29:53 +0800

    removed HelloWorld.vue

--- cafe-server/src/App.vue
+++ cafe-server/src/App.vue
@@ -5,13 +5,8 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
